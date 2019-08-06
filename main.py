76267ff500d81d56043e837530724d3982681292
Danny Kilkenny <danny.kilkenny18@gmail.com>
2019-08-06 15:41:36 -0400

    added untethered

--- main.py
+++ main.py
@@ -2,10 +2,10 @@
 
 # Bokeh Imports
 from bokeh.io import output_file, show
-from bokeh.layouts import column
+from bokeh.layouts import row, column
 from bokeh.plotting import figure
 from bokeh.layouts import gridplot
-from bokeh.models import Axis
+from bokeh.models import Axis, Slider
 
 # Misc Imports
 import numpy as np
@@ -148,16 +148,21 @@ N = 500
 x = np.linspace(0, 100, N)
 y = np.linspace(0, 100, N)
 
-p = figure(tooltips=[("x", "$x"), ("y", "$y"), ("value", "@image")])
-p.x_range.range_padding = 0 
-p.y_range.range_padding = 0
-p.plot_width = 500
-p.plot_height = 500
-p.xaxis.axis_label = "Mach"
-p.yaxis.axis_label = "Altitude"
-p.min_border_left = 100
+
+mach_slider = Slider(start=min(mach), end=max(mach), value=0, step=0.05, title="Mach")
+alt_slider = Slider(start=min(alt), end=max(alt), value=0, step=1, title="Altitude")
+throttle_slider = Slider(start=min(throttle), end=max(throttle), value=0, step=0.05, title="Throttle")
+
+contour_plot = figure(tooltips=[("x", "$x"), ("y", "$y"), ("value", "@image")])
+contour_plot.x_range.range_padding = 0
+contour_plot.y_range.range_padding = 0
+contour_plot.plot_width = 500
+contour_plot.plot_height = 500
+contour_plot.xaxis.axis_label = "Mach"
+contour_plot.yaxis.axis_label = "Altitude"
+contour_plot.min_border_left = 100
 # must give a vector of image data for image parameter
-p.image(image=[Z], x=0, y=0, dw=max(mach), dh=max(alt), palette="Viridis11")
+contour_plot.image(image=[Z], x=0, y=0, dw=max(mach), dh=max(alt), palette="Viridis11")
 
 
 # create a new plot
@@ -172,56 +177,12 @@ s3.xaxis.axis_label = "Mach"
 s3.yaxis.axis_label = "Thrust"
 s3.square(x, y2, size=10, color="olive", alpha=0.5)
 
+
+layout = row(
+    column(mach_slider, alt_slider, throttle_slider),
+)
 # make a grid
-grid = gridplot([[s1, p], [None, s3]])
+grid = gridplot([[s1, contour_plot, layout], [None, s3]])
 
 # show the results
 show(grid)
-
-
-
-
-
-
-from bokeh.layouts import row, column
-from bokeh.models import CustomJS, Slider
-from bokeh.plotting import figure, output_file, show, ColumnDataSource
-
-x = np.linspace(0, 10, 500)
-y = np.sin(x)
-
-source = ColumnDataSource(data=dict(x=x, y=y))
-
-plot = figure(y_range=(-10, 10), plot_width=400, plot_height=400)
-
-plot.line('x', 'y', source=source, line_width=3, line_alpha=0.6)
-
-amp_slider = Slider(start=0.1, end=10, value=1, step=.1, title="Amplitude")
-freq_slider = Slider(start=0.1, end=10, value=1, step=.1, title="Frequency")
-phase_slider = Slider(start=0, end=6.4, value=0, step=.1, title="Phase")
-offset_slider = Slider(start=-5, end=5, value=0, step=.1, title="Offset")
-
-callback = CustomJS(args=dict(source=source, amp=amp_slider, freq=freq_slider, phase=phase_slider, offset=offset_slider),
-                    code="""
-    const data = source.data;
-    const A = amp.value;
-    const k = freq.value;
-    const phi = phase.value;
-    const B = offset.value;
-    const x = data['x']
-    const y = data['y']
-    for (var i = 0; i < x.length; i++) {
-        y[i] = B + A*Math.sin(k*x[i]+phi);
-    }
-    source.change.emit();
-""")
-
-amp_slider.js_on_change('value', callback)
-freq_slider.js_on_change('value', callback)
-phase_slider.js_on_change('value', callback)
-offset_slider.js_on_change('value', callback)
-
-layout = row(
-    plot,
-    column(amp_slider, freq_slider, phase_slider, offset_slider),
-)
